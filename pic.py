75b13adca1e9e227d5d379e5637d546c8c2258d6
Kevin <kevin.gonzalez98@yahoo.com>
2018-11-08 18:30:48 -0500

    Pic fix

--- pic.py
+++ pic.py
@@ -2,16 +2,19 @@ from PIL import Image, ImageDraw, ImageFont
 
 def createImg():
 
-    W, H = (20,20)
-    msg = "MOON"
+    msg = "The quick brown fox jumps over the LAZY dog!"
+    fontSize = 10
 
-    myFont = ImageFont.truetype('fonts/OpenSans-Bold.ttf', 30)
+    myFont = ImageFont.truetype('LucidaTypewriterBold.ttf', fontSize) #user
 
-    for x in msg:  # create a black and white image for each character
-        letter = Image.new("L", (W, H))  # grayscale image, had to draw bitmap
-        # because it was the only way to center the character, other options were not working properly
+
+#Letter creation(Dimensions, msg, font)
+    for x in msg:  # create a black and white image for each character; x => letter
+        #ForL : it was the only way to center the character, other options were not working properly
         mask = myFont.getmask(x)
-        mask_w, mask_h = mask.size
+        mask_w, mask_h = mask.size #Letter size
+        W,H = mask_w + 2, mask_h + 2 #FIXME:W,H should be largest mask created Execption on space
+        letter = Image.new("L", (W,H)) # grayscale image, had to draw bitmap
         d = Image.core.draw(letter.im, 0)
         d.draw_bitmap(((W - mask_w)/2, (H - mask_h)/2), mask, 255)  # 255 is pixel intensity of text
         letter.save(x+'.png')
@@ -28,15 +31,15 @@ def createImg():
         letter.putdata(new_pixels)  # copies pixel data into character
         letter.save("Transparent "+x+".png", "PNG")  # save transparent character
 
-    background = Image.open('moon.jpg')
+    background = Image.open('landscape.jpg')
 
     width, height = background.size
 
     v_position = 0
     letter_index = 0
     msg_length = len(msg)
-    columns = width/20
-    rows = height/20
+    columns = width/W
+    rows = height/H
 
     for r in range(0, int(round(rows))):
         h_position = 0
@@ -46,9 +49,9 @@ def createImg():
 
             foreground = Image.open("Transparent "+msg[letter_index]+".png")
             background.paste(foreground, (h_position, v_position), foreground)
-            h_position = h_position + 20
+            h_position = h_position + W
             letter_index = letter_index + 1
-        v_position = v_position + 20
+        v_position = v_position + H
 
     background.save('result.png')
 

827e1584096c4731c36bb0c18cb8ec358c01a81b
Kevin <kevin.gonzalez98@yahoo.com>
2018-11-29 18:00:33 -0500

    FIXME

--- pic.py
+++ pic.py
@@ -2,10 +2,17 @@
 from PIL import Image, ImageDraw, ImageFont
 import CharImg, ImgPlacement
 
-msg = "Hello Katelyn, how are you enjoying once upon a time?"
-fontSize = 25
-style = 0
-image = Image.open("SnowLeopard.png")
+msg = "DRMONIQUEROSS"
+fontSize = 45
+#small 25 a.jpg
+#medium 35 on a.jpg 
+#Large 45 on a
+#Make spaces transparent
+#Fix border: a) test letter overflow (First letter MUST BE whole)
+#            b) If disgusting center letters
+#bad words
+style = 2
+image = Image.open("a.jpg")
 myFont = ImageFont.truetype('FreeMonoBold.ttf', fontSize) #user
 
 (charWidth, charHeight), charImgs = CharImg.createCharacterImgs(msg, myFont)
@@ -13,4 +20,12 @@ charImgs = CharImg.styleCharImgs(charImgs, style)
 final = ImgPlacement.charOnImg(charImgs, image)
 
 final.save('result.png')
+
+image = Image.open("rainbow.jpg")
+(charWidth, charHeight), charImgs = CharImg.createCharacterImgs(msg, myFont)
+charImgs = CharImg.styleCharImgs(charImgs, style)
+final = ImgPlacement.charOnImg(charImgs, image)
+final.save('result2.png')
+
+
 
